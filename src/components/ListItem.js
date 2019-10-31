fe06c87267baea5b9b3a4a35813776a1371b76d7
Николай Инкогнито <mmmsvit@gmail.com>
2019-11-01 00:58:28 +0200

    init

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,27 +4,28 @@ import {Button, ListGroup} from 'react-bootstrap';
 
 // https://stackoverflow.com/questions/29810914/react-js-onclick-cant-pass-value-to-method
 
-export const ListItem2 = ({item, handleItemClick, ...props}) => {
-    return (
-        <Button className="list-group-item list-group-item-action" onClick={()=>handleItemClick(item)} >{item.name}</Button>
-    )
-};
-
-
-
 export const ListItem = ({item, handleItemClick, ...props}) => {
     return (
-        <ListGroup.Item action
-                        href={`#${item.id}`}
-                        key={item.id}
 
-                        onClick={()=>handleItemClick(item)}>
-            {item.name}
-        </ListGroup.Item>
+        <Button className="list-group-item list-group-item-action" onClick={() => handleItemClick(item)}>{item.name}</Button>
     )
 };
 
 
+// export const ListItem2 = ({item, handleItemClick, ...props}) => {
+//     return (
+//         <ListGroup.Item action
+//                         href={`#${item.id}`}
+//                         key={item.id}
+
+//                         onClick={()=>handleItemClick(item)}>
+//             {item.name}
+//         </ListGroup.Item>
+//     )
+// };
+
+
+// NOT WORK with ListGroup.Item !!!!!!!!!!
 // export const ListItem = ({item, handleItemClick, ...props}) => {
 //     return (
 //         <ListGroup.Item action
